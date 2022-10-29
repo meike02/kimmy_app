126206d53eccaaccc87c25dadfbef7b57a981981
lilpard <61854819+meike02@users.noreply.github.com>
2022-10-29 19:55:13 +0800

    删除编译文件

--- out/production/kimmy/lib/data/store/server_list_controller.dart
+++
@@ -1,6 +0,0 @@
-import '../../core/data/box_controller.dart';
-import '../model/server_info.dart';
-
-class ServerListController extends BoxController<ServerInfo>{
-  ServerListController():super();
-}
--- out/production/kimmy/lib/data/store/sshkey_list_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-
-import 'package:kimmy/core/data/box_controller.dart';
-
-import '../model/sshkey_info.dart';
-
-class SSHKeyListController extends BoxController<SSHKeyInfo>{
-  SSHKeyListController() : super();
-}
